de6f8fe9447f5f47fe55ff7290fdfddfdd061981
Michela <m.gaeta29@studenti.unisa.it>
2026-03-31 18:10:41 +0200

    Adding history view- communication

--- calculator.py
+++ calculator.py
@@ -3,9 +3,16 @@ import memory_handler
 memory=memory_handler.Memory()
 parser=expression_parser.ExpressionParser(memory)
 while(True): 
-    print("Insert your expression here:")
-    expression=input()
-    result=parser.parse(expression)  
-    print("Result:", result)
-    if isinstance(result, (int, float)):
-        memory.save(expression,result)
+    print ("Select an operation:\n1.Evaluate Expression\n2.See history")
+    choice=input()
+    if choice=='1':
+        print("Insert your expression here:")
+        expression=input()
+        result=parser.parse(expression)  
+        print("Result:", result)
+        if isinstance(result, (int, float)):
+            memory.save(expression,result)
+    elif choice=='2': 
+        print(memory.show_history())
+    else: 
+        print("Invalid choice")
